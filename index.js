7f58677e1b3a790a4d7361e828c59a70a52db753
Salar Abdefaily <salar118@yahoo.com>
2018-11-27 17:55:08 +0100

    adding google  signin possibility

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 var expressGraphGL = require('express-graphql');
-
 const schema = require('./routes/graphql/schema');
 
+const keys = require('./config/keys');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientId,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, accessToken => {
+  console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 app.use('/graphql', expressGraphGL({
   schema,
   graphiql: true
