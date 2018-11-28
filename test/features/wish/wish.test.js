ea9d2e8ca42292d24eeafcde3b21f6083a3b316e
Salar Abdefaily <salar118@yahoo.com>
2018-11-28 19:18:51 +0100

    adding debug to monoMemoryserver

--- test/features/wish/wish.test.js
+++ test/features/wish/wish.test.js
@@ -9,7 +9,9 @@ let mongoServer;
 const opts = {};
 
 beforeAll(async () => {
-  mongoServer = new MongoMemoryServer();
+  mongoServer = new MongoMemoryServer({
+    debug: true
+  });
   const mongoUri = await mongoServer.getConnectionString();
   await mongoose.connect(mongoUri, opts, (err) => {
     if (err) console.error(err);
@@ -23,14 +25,14 @@ afterAll(() => {
 });
 
 describe("Wish", () => {
-    it("should save a wish", async () => {
-      const newWish = new Wish({
-          title: 'title wish sd ',
-          story: 'story of wish'
-        });
-        await newWish.save();
-
-        const cnt = await Wish.count();
-        expect(cnt).toBe(1);
+  it("should save a wish", async () => {
+    const newWish = new Wish({
+      title: 'title wish sd ',
+      story: 'story of wish'
     });
+    await newWish.save();
+
+    const cnt = await Wish.count();
+    expect(cnt).toBe(1);
+  });
 });
