fdc56d23573ce0b82ad870e28151ea86d8022676
Salar Abdefaily <salar118@yahoo.com>
2018-12-07 19:21:34 +0100

    adding jshintrc

--- routes/graphql/resolvers.js
+++ routes/graphql/resolvers.js
@@ -5,7 +5,6 @@ module.exports = {
         returnWish: async (parent, {}, context, info) => {
             const wish = await wishFeature.queries.createWish();
             console.log('wish will be created' + id);
-
             return wish;
         }
     }
